0e82f2463bc018e178e08f647a3e98b0e0b6f020
Whitny Edwards <wne4e2@gmail.com>
2019-02-09 21:56:06 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,17 +1,21 @@
 CREATE TABLE projects (
-id INTEGER PRIMARY KEY,
-title TEXT,
-category TEXT,
-funding goal REAL,
-start_date INTEGER,
-end_date INTEGER);
-
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  category TEXT,
+  funding_goal INTEGER,
+  start_date TEXT,
+  end_date TEXT
+);
 
 CREATE TABLE users (
-id INTEGER PRIMARY KEY,
-name TEXT,
-age INTEGER);
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER
+);
 
 CREATE TABLE pledges (
-id INTEGER PRIMARY KEY,
-amount REAL);
+  id INTEGER PRIMARY KEY,
+  amount INTEGER,
+  user_id INTEGER,
+  project_id INTEGER
+);
