0a694b49810dd3e0796c9e949f933d98236e5f6c
Whitny Edwards <wne4e2@gmail.com>
2019-02-09 21:45:53 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -4,4 +4,14 @@ title TEXT,
 category TEXT,
 funding goal REAL,
 start_date INTEGER,
-end_date INTEGER)
+end_date INTEGER);
+
+
+CREATE TABLE users (
+id INTEGER PRIMARY KEY,
+name TEXT,
+age INTEGER);
+
+CREATE TABLE pledges (
+id INTEGER PRIMARY KEY,
+amount REAL);
